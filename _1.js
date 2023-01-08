55086936dc64bbec83a263eb8d2a54de787f04aa
imapotatoes11 <79340132+imapotatoes11@users.noreply.github.com>
2023-01-08 10:51:47 -0500

    minor changes

--- _1.js
+++ _1.js
@@ -33,7 +33,18 @@ function httpGet(url){
         xhr.send();
     });
 }
-async function makeGetRequest(url){
+// make a http get request to the url
+// and return the response as a string
+function makeGetRequest(url){
+    return new Promise((resolve, reject)=>{
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', url);
+        xhr.onload = ()=>resolve(xhr.responseText);
+        xhr.onerror = ()=>reject(xhr.statusText);
+        xhr.send();
+    });
+}
+/*async function makeGetRequest(url){
     try{
         const response = await fetch(url,{mode: 'no-cors',method: 'GET'});
         const text=await response.json();
@@ -42,7 +53,7 @@ async function makeGetRequest(url){
     } catch (error){
         console.log(error);
     }
-}
+}*/
 function _makeGetRequest(url){
     /*return makeGetRequest(url).then((text)=>{
         return text;
